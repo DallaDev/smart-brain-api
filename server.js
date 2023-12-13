281282dc925075b5cc73b1e8fe4651ecdb909823
DallaDev <danniella_ser@yahoo.com>
2023-12-13 11:33:44 -0500

    ssl updated

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
       // host : '127.0.0.1',
       // port : 5432,
       // user : 'postgres',
