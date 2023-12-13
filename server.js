fa127c2eeb7eb5827a3bc2ccae09562eb9558d08
DallaDev <danniella_ser@yahoo.com>
2023-12-13 12:21:30 -0500

    const knex update

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      },
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : '',
-      database : 'smart-brain'
+      ssl: { rejectUnauthorized: false }
+      // host : '127.0.0.1',
+      // port : 5432,
+      // user : 'postgres',
+      // password : '',
+      // database : 'smart-brain'
     }
   });
 
