37ac5fdcf8e2a58cb481083d96814cb3f9cdab9c
DallaDev <danniella_ser@yahoo.com>
2023-12-15 02:32:11 -0500

    updated connection to backend in server.js

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false}
-      // host : '127.0.0.1',
-      // port : 5432,
-      // user : 'postgres',
-      // password : '',
-      // database : 'smart-brain'
+      ssl: { rejectUnauthorized: false},
+      host : process.env.DATABASE_HOST,
+      port : 5432,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
   });
 
